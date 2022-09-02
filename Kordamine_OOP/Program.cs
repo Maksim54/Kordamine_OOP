7b651b55b6417f3760879801b84c4832f25c9200
opilane <opilane@HP-CZC6297B8C>
2022-09-02 08:40:30 +0300

    Add project files.

---
+++ Kordamine_OOP/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kordamine_OOP;
+Console.WriteLine("Hello, World!");
+Koer koer = new Koer(Koer.toug.taksa, "Sarik", "punane", Koduloom.sugu.isane, 35.5, 7, true);
+koer.print_Info();
+koer.print_Haal();
+Koer veelkoer = new Koer(koer);
+veelkoer.muudaNimi("Barsik");
+veelkoer.muudaVarv("Hall");
+veelkoer.muudaKaal(40);
+veelkoer.muudaVanus(5);
+
+veelkoer.print_Info();
